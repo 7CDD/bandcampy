667b3a0eedddeeae0bbe102b23c663da7f6eaeb8
Todd Culver <tawddd@gmail.com>
2020-10-10 09:39:01 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Bandcampy
+
+We're cruisin on our scraper bike
+
+## INSTALL
+  get the version of chromedriver that matches your chrome install
+    https://chromedriver.chromium.org/downloads
+
+  pip install selenium
+  pip install beautifulsoup4
---
+++ bandcampy.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+# Bandcamp URL parameters
+# --------------------------------------
+# genre   - all
+# sort_by - top = (best-selling),
+#           rec = (artist-recommended),
+#           new = (new arrivals)
+# r       - most, latest
+
+URL = 'https://bandcamp.com/?g={genre}&s={sort_by}&p=0&gn=0&f=all&w=0&r={r}'.format(
+    genre="all", sort_by="rec", r="most")
+
+# hide a warning about a bluetooth error...
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+# GET request
+DRIVER_PATH = Path("./bandcampy/chromedriver.exe")
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get(URL)
+
+# make soup
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+artist_list = soup.find_all("a", {"class": "item-artist"})
+
+for artist in artist_list:
+    print(artist.text)
+
+# close webdriver
+driver.quit()
